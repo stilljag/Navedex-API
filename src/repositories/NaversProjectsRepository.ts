58c740b7d102bc33dd8c04694fd236fa5ab436b2
william ribeiro <william_jag_rs@hotmail.com>
2021-03-03 16:42:57 -0300

    Create NaversProjectsRepository.ts

---
+++ src/repositories/NaversProjectsRepository.ts
@@ -0,0 +1,8 @@
+import { EntityRepository, Repository } from "typeorm";
+
+import { NaverProject } from "../models/NaverProject";
+
+@EntityRepository(NaverProject)
+class NaversProjectsRepository extends Repository<NaverProject> {}
+
+export { NaversProjectsRepository };
