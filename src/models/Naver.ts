b798fb100bc1c6caaf8ae8e9104e94f6426c92af
william ribeiro <william_jag_rs@hotmail.com>
2021-03-03 16:09:45 -0300

    Create models-Naver.ts

---
+++ src/models/Naver.ts
@@ -0,0 +1,31 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("navers")
+class Naver {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  birthdate: string;
+
+  @Column()
+  admission_date: string;
+
+  @Column()
+  job_role: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Naver };
