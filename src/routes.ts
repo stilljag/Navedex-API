d16b153ce4c3541a4eb1432ab9850de783df3592
william ribeiro <william_jag_rs@hotmail.com>
2021-03-03 16:08:24 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
+import { SessionController } from "./controllers/SessionController";
 
 const router = Router();
 
 const userController = new UserController();
+const sessionController = new SessionController();
 
 router.post("/signup", userController.store);
+router.post("/login", sessionController.store);
 
 export { router };
