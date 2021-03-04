7e1970d78d0a5f270329abe57b4161dc6cae1daf
william ribeiro <william_jag_rs@hotmail.com>
2021-03-04 00:37:50 -0300

    Create Middleware auth.ts

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+import { AppError } from "../errors/AppError";
+export default (request: Request, response: Response, next: NextFunction) => {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("No token provided", 401);
+  }
+
+  const [scheme, token] = authHeader.split(" ");
+
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
+    if (err) throw new AppError("Token Invalid");
+
+    request.userId = decoded.sub;
+
+    return next();
+  });
+};
