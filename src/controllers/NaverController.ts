498e2e47e8698b4c7094ab6f4cb6094f7a0ccc5b
william ribeiro <william_jag_rs@hotmail.com>
2021-03-06 14:19:28 -0300

    Implement methods *update

--- src/controllers/NaverController.ts
+++ src/controllers/NaverController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
+import {
+  createQueryBuilder,
+  getConnection,
+  getCustomRepository,
+} from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
 import { NaversRepository } from "../repositories/NaversRepository";
 import { NaversProjectsRepository } from "../repositories/NaversProjectsRepository";
@@ -83,6 +87,91 @@ class NaverController {
     // await naversProjectRepository.save(saveProject); , saveProject
 
     return response.status(201).json({ naver });
+  }
+
+  async update(request: Request, response: Response) {
+    const {
+      name,
+      birthdate,
+      admission_date,
+      job_role,
+      projects,
+    } = request.body;
+
+    const { id, naver_id } = request.params;
+
+    //validações
+    const schema = yup.object().shape({
+      name: yup.string().required("Nome é obrigatório"),
+      birthdate: yup.string().required("Data de nascimento é obrigatório"),
+      admission_date: yup.string().required("Data de admissão é obrigatório"),
+      job_role: yup.string().required("Função é obrigatório"),
+      //projects: yup.string().required("Projeto é obrigatório"),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (error) {
+      throw new AppError(error.errors);
+    }
+
+    //verifica se o usuario existe
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userExists = await usersRepository.findOne({
+      id: String(id),
+    });
+
+    if (!userExists) {
+      throw new AppError("User not found!!");
+    }
+
+    //verificar se o token é do usuario
+    if (process.env.DECODED != id) {
+      throw new AppError("Token does not belong to the user!!", 401);
+    }
+
+    //verifica se os projetos existem
+    const projectsRepository = getCustomRepository(ProjectsRepository);
+
+    const projectsExists = await projectsRepository.findByIds(projects);
+
+    if (projectsExists.length != projects.length) {
+      throw new AppError("Project not found!!");
+    }
+
+    //verifica se o Naver existe
+    const naversRepository = getCustomRepository(NaversRepository);
+
+    const naverExists = await naversRepository.findOne({
+      id: String(naver_id),
+    });
+
+    if (!naverExists) {
+      throw new AppError("Naver not found!!");
+    }
+
+    if (naverExists.user_id != id)
+      throw new AppError("Naver does not belong to the user!!");
+
+    try {
+      const naver = await createQueryBuilder()
+        .update("navers")
+        .set({
+          name,
+          birthdate,
+          admission_date,
+          job_role,
+        })
+        .where("id = :id", { id: naverExists.id })
+        .execute();
+
+      return response
+        .status(200)
+        .json({ "Message >>": "Naver successfully update!" });
+    } catch (error) {
+      throw new AppError(error.errors);
+    }
   }
 
   async index(request: Request, response: Response) {
@@ -201,7 +290,9 @@ class NaverController {
       throw new AppError(error);
     }
 
-    return response.status(200).json("Naver successfully deleted!");
+    return response
+      .status(200)
+      .json({ "Message >>": "Naver successfully deleted!" });
   }
 }
 
